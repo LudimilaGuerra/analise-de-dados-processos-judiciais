0c2652e2ebf2bd902fc1d106dd5fb143c7ac0ee4
GBarbosa <andradegui2006@gmail.com>
2025-04-07 15:25:34 -0300

    Update processo.c

--- processo.c
+++ processo.c
@@ -1,3 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 #include "processo.h"
+
+void ordenarPorId(Processo processos[], int n){
+//1. Ordenar, em ordem crescente, o conjunto de dados a partir do atributo “id”;
+}
+
+void ordenarPorData(Processo processos[], int n){
+//2. Ordenar, em ordem decrescente, o conjunto de dados a partir do atributo “data_ajuizamento”;
+}
+
+int contarPorClasse(Processo processos[], int n, int id_classe){
+//3. Contar quantos processos estão vinculados a um determinado “id_classe”;
+}
+
+int contarAssuntosUnicos(Processo processos[], int n){
+//4. Identificar quantos “id_assuntos” constam nos processos presentes na base de dados;
+}
+
+void listarMultiplosAssuntos(Processo processos[], int n){
+//5. Listar todos os processos que estão vinculados a mais de um assunto; e
+}
+
+int calcularDiasTramitando(Processo p, const char* data_atual){
+//6. Indicar a quantos dias um processo está em tramitação na justiça.
+}
+
+
+int main() {
+
+}
