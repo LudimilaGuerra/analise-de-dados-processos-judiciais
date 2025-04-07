48a1b9a9ab2bc78a48c99a65710443e8adf37082
GBarbosa <andradegui2006@gmail.com>
2025-04-07 15:46:43 -0300

    Update processo.c

--- processo.c
+++ processo.c
@@ -3,27 +3,29 @@
 #include "processo.h"
 
 void ordenarPorId(Processo processos[], int n){
-//1. Ordenar, em ordem crescente, o conjunto de dados a partir do atributo “id”;
+    //1. Ordenar, em ordem crescente, o conjunto de dados a partir do atributo “id”;
 }
 
 void ordenarPorData(Processo processos[], int n){
-//2. Ordenar, em ordem decrescente, o conjunto de dados a partir do atributo “data_ajuizamento”;
+    //2. Ordenar, em ordem decrescente, o conjunto de dados a partir do atributo “data_ajuizamento”;
 }
 
 int contarPorClasse(Processo processos[], int n, int id_classe){
-//3. Contar quantos processos estão vinculados a um determinado “id_classe”;
+    //3. Contar quantos processos estão vinculados a um determinado “id_classe”;
 }
 
 int contarAssuntosUnicos(Processo processos[], int n){
-//4. Identificar quantos “id_assuntos” constam nos processos presentes na base de dados;
+    //4. Identificar quantos “id_assuntos” constam nos processos presentes na base de dados;
 }
 
 void listarMultiplosAssuntos(Processo processos[], int n){
-//5. Listar todos os processos que estão vinculados a mais de um assunto; e
+    //5. Listar todos os processos que estão vinculados a mais de um assunto; e
 }
 
 int calcularDiasTramitando(Processo p, const char* data_atual){
-//6. Indicar a quantos dias um processo está em tramitação na justiça.
+    //6. Indicar a quantos dias um processo está em tramitação na justiça.
+    //É mais usável se verificando todo o arquivo de processos em busca do ID, mas é mais fácil criar passando Processo p, const char* data_atual
+    //Passar como parâmetro a data atual ou usar a biblioteca time.h pra fazer isso automaticamente
 }
 
 
