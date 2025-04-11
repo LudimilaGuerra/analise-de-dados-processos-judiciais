d6c7f7dd23a679bec8d4bbd1ca3186acda2123fd
Pedro Melo <pedrolucaslopesmelo@gmail.com>
2025-04-10 22:10:57 -0300

    Update processo.c

--- processo.c
+++ processo.c
@@ -231,7 +231,8 @@ void limparQuebraLinha(char* str) {
         str[--len] = '\0';
     }
 }
-void listar_processos_varios_assuntos(Processo processos[], int total) {
+void listar_processos_varios_assuntos(Processo processos[], int total)
+ {
     printf("\nProcessos com mais de um assunto:\n");
     printf("==================================\n");
     for (int i = 0; i < total; i++) {
